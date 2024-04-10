35473f447e59b068e0687fc18865b11037417183
Omelchenko Michael <socanime@gmail.com>
2024-04-10 13:59:28 +0300

    deleted swagger_js_url

--- src/configs/development/settings.py
+++ src/configs/development/settings.py
@@ -10,5 +10,3 @@ class DevelopmentAppSettings(AppSettings):
     app_name: str = "My application in development mode."
     title: str = "My config"
     environment: Optional[str] = EnvironmentType.DEVELOPMENT
-    swagger_js_url: str = "https://cdn.jsdelivr.net/npm/swagger-ui-dist@5.14.0/swagger-ui-bundle.js"
-    # swagger_css_url: str = "https://cdn.jsdelivr.net/npm/swagger-ui-dist@5.15.0/swagger-ui.css"
